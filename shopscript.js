340ab2a7a4619c2a7929dc43113caadcb7ec0367
Grunkk <sigurd6260@gmail.com>
2023-01-17 15:51:10 +0100

    Nice

---
+++ shopscript.js
@@ -0,0 +1,29 @@
+let cart = []
+
+function addToCart(title, price) {
+    cart.push({productTitle: title, productPrice: price, productQuantity: 1})
+
+    console.log(cart)
+    //etter å ha lagt til et produkt; oppdater handlelistevisning
+    renderCart()
+    //Oppdatere label med riktig antall produkter
+    document.querySelector("#cart .label").innerHTML = cart.length
+}
+
+function renderCart() {
+    //Tom variabel for å bygge HTML til produkter
+    let listHTML = ""
+    //Gå gjennom cart-arrayen, lag <li> for hvert produkt
+    cart.map(prod => listHTML += `<li>
+    <span class="title">${prod.productTitle}</span>
+    <span class="price">${prod.productPrice}</span>
+    <span class="quantity">${prod.productQuantity}</span>
+    <button class="delete">X</button>
+</li>`)
+    //Bruke en selector for å finne riktig <ul>, og skrive inn listHTML
+    document.querySelector("#cartview ul").innerHTML = listHTML
+}
+
+function toggleCart() {
+    document.querySelector("#cartview").classList.toggle("hidden")
+}
